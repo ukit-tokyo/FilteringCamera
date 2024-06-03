78345ea14768403502f04a7e11fc1ef8e53470ab
ukit-tokyo <t.yuki@ukit.tokyo>
2024-06-03 17:10:31 +0900

    Create project

---
+++ FilteringCamera/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  FilteringCamera
+//
+//  Created by Taichi Yuki on 2024/06/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
+
