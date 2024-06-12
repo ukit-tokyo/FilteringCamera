67679c23144f9c330964c65533daf2258096a731
ukit-tokyo <t.yuki@ukit.tokyo>
2024-06-12 11:46:18 +0900

    refactor photo edit canvas

--- FilteringCamera/PhotoEditViewController.swift
+++ FilteringCamera/PhotoEditViewController.swift
@@ -8,49 +8,15 @@
 import UIKit
 
 final class PhotoEditViewController: UIViewController {
-  private lazy var imageView: UIImageView = {
+  private let imageView: UIImageView = {
     let imageView = UIImageView()
     imageView.contentMode = .scaleAspectFit
     return imageView
   }()
 
-  private lazy var scrollView: UIScrollView = {
-    let scrollView = UIScrollView()
-    scrollView.showsVerticalScrollIndicator = false
-    scrollView.showsHorizontalScrollIndicator = false
-    scrollView.minimumZoomScale = 1
-    scrollView.maximumZoomScale = 2
-//    scrollView.clipsToBounds = false
-    return scrollView
-  }()
-
-  private var overlay: UIView = {
-    let view = UIView()
-    view.backgroundColor = .black
-    view.alpha = 0.5
-    view.isUserInteractionEnabled = false
-    return view
-  }()
-
-  private let image: UIImage
-
-  private let screenWidth = UIScreen.main.bounds.width
-  private let screenHeight = UIScreen.main.bounds.height
-  private let horizontalInset: CGFloat = 16
-
-  var beforeVC = "ChangeProfileVC"
-
-  var cropArea: CGRect {
-    return CGRect(
-      x: horizontalInset,
-      y: screenHeight / 2 - (screenWidth - (horizontalInset * 2)) / 2,
-      width: screenWidth - (horizontalInset * 2),
-      height: screenWidth - (horizontalInset * 2))
-  }
-
   init(image: UIImage) {
-    self.image = image
     super.init(nibName: nil, bundle: nil)
+    imageView.image = image
   }
   
   required init?(coder: NSCoder) {
@@ -64,119 +30,33 @@ final class PhotoEditViewController: UIViewController {
       self.dismiss(animated: true, completion: nil)
     })
 
-    let imageWidth = image.size.width
-    let imageHeight = image.size.height
+    view.backgroundColor = .black
 
-    if imageWidth < imageHeight { // 縦長画像
-      let ratio = imageHeight / imageWidth
-      imageView.frame.size = CGSize(width: screenWidth - (horizontalInset * 2), height: (screenWidth - (horizontalInset * 2)) * ratio)
-      // scrollViewのサイズ設定 スクロール操作可能エリアを広げるために十分に高さを確保する
-      scrollView.frame.size = CGSize(width: imageView.frame.width, height: imageView.frame.width * 1.5)
-      let inset = (scrollView.frame.height - imageView.frame.width) / 2
-      scrollView.contentInset.top = inset
-      scrollView.contentInset.bottom = inset
-    } else { // 横長画像
-      let ratio = imageWidth / imageHeight
-      imageView.frame.size = CGSize(width: (screenWidth - (horizontalInset * 2)) * ratio, height: screenWidth - (horizontalInset * 2))
-      scrollView.frame.size = CGSize(width: screenWidth, height: imageView.frame.height)
-      scrollView.contentInset.left = horizontalInset
-      scrollView.contentInset.right = horizontalInset
+    let canvasView = UIView()
+    canvasView.backgroundColor = .clear
+
+    let toolView = UIView()
+    toolView.backgroundColor = .clear
+
+    view.addSubview(canvasView)
+    canvasView.snp.makeConstraints { make in
+      make.top.equalTo(view.safeAreaLayoutGuide)
+      make.left.right.equalToSuperview()
     }
 
-    scrollView.contentSize = imageView.frame.size
-    scrollView.center = view.center
-
-    // imageViewにライブラリで選択した画像を入れる
-    imageView.image = image
-
-    scrollView.delegate = self
-
-    scrollView.addSubview(imageView)
-    view.addSubview(scrollView)
-
-    addCropView()
-    setUpBorder()
-  }
-
-  // クロップ範囲を表示するViewの作成
-  func addCropView() {
-    overlay.frame = view.bounds
-
-    // クロップする範囲で図形を作る（丸型にする）
-    let cropAreaPath = UIBezierPath(roundedRect: cropArea, cornerRadius: view.frame.width / 2)
-    // 外枠の範囲で図形を作る（背景が黒になる範囲）
-    let outsideCropAreaPath = UIBezierPath(rect: scrollView.frame)
-    // クロップ範囲の丸型図形の下にViewの幅の図形を追加する
-    cropAreaPath.append(outsideCropAreaPath)
-
-    let cropAreaLayer = CAShapeLayer()
-    // cropAreaPathの持つ図形のpathを渡す
-    cropAreaLayer.path = cropAreaPath.cgPath
-    // クロップするエリアの塗り潰し解除
-    cropAreaLayer.fillRule = .evenOdd
-    overlay.layer.mask = cropAreaLayer
-
-    view.addSubview(overlay)
-  }
-
-  // クロップ範囲の枠を作成
-  func setUpBorder() {
-    let border = CAShapeLayer()
-    let borderPath = UIBezierPath(rect: cropArea)
-    border.path = borderPath.cgPath
-    border.lineWidth = 1
-    border.strokeColor = UIColor.white.cgColor
-    overlay.layer.addSublayer(border)
-  }
-
-  @IBAction func pushCropButton(_ sender: Any) {
-
-    // クロップ領域の位置とサイズを取得
-    let cropAreaRect = overlay.convert(cropArea, to: scrollView)
-
-    // 画像がどのくらい拡大されたかの倍率
-    let imageViewScale = max(
-      image.size.width / imageView.frame.width,
-      image.size.height / imageView.frame.height
-    )
-
-    // クロップする範囲を定義
-    let cropZone = CGRect(
-      x: (cropAreaRect.origin.x - imageView.frame.origin.x) * imageViewScale,
-      y: (cropAreaRect.origin.y - imageView.frame.origin.y) * imageViewScale,
-      width: cropAreaRect.width * imageViewScale,
-      height: cropAreaRect.height * imageViewScale
-    )
-
-    let croppedCGImage = image.cgImage?.cropping(to: cropZone)
-
-    if let croppedCGImage = croppedCGImage {
-      // 元の画像の向きを元に向きを再調整する
-      let croppedImage = UIImage(cgImage: croppedCGImage, scale: 0, orientation: image.imageOrientation)
-
-//      if beforeVC == "ChangeProfileVC" {
-//        let changeProfileVC = navigationController?.viewControllers.first(where: {$0 is ChangeProfileViewController}) as! ChangeProfileViewController
-//        changeProfileVC.customView.icon.image = croppedImage
-//
-//        navigationController?.popToViewController(changeProfileVC, animated: true)
-//      } else if beforeVC == "ProfileModificationVC" {
-//        let profileModificationVC = navigationController?.viewControllers.first(where: {$0 is ProfileModificationViewController}) as! ProfileModificationViewController
-//        profileModificationVC.customView.icon.image = croppedImage
-//
-//        navigationController?.popToViewController(profileModificationVC, animated: true)
-//      }
-
+    view.addSubview(toolView)
+    toolView.snp.makeConstraints { make in
+      make.top.equalTo(canvasView.snp.bottom)
+      make.left.right.equalToSuperview()
+      make.bottom.equalTo(view.safeAreaLayoutGuide)
+      make.height.equalTo(160)
     }
-  }
 
-//  // 戻るボタンが押された時の処理
-//  @IBAction func pushBackButton(_ sender: Any) {
-//    self.navigationController?.popViewController(animated: true)
-//  }
-}
-
-extension PhotoEditViewController: UIScrollViewDelegate {
-  func viewForZooming(in scrollView: UIScrollView) -> UIView? {
-    return imageView
+    canvasView.addSubview(imageView)
+    imageView.snp.makeConstraints { make in
+      make.center.equalToSuperview()
+      make.width.equalToSuperview().inset(16)
+      make.height.equalTo(imageView.snp.width)
+    }
   }
 }
